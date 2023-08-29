b35cf662fec67a00464f3c1888714ae3f294a5b8
Dmytro Sukhyi <dmytro.sukhiy@gmail.com>
2023-08-29 23:25:56 +0100

    clean

--- ios/Rapidsnark.mm
+++ ios/Rapidsnark.mm
@@ -3,18 +3,9 @@
 
 #import <React/RCTLog.h>
 
-// #import  <React/RCTBridge+Private.h>
-// #import <jsi/jsi.h>
-// #import <React/RCTUtils.h>
-// #import <ReactCommon/CallInvoker.h>
-
-// #import <memory>
-
 @implementation Rapidsnark
 RCT_EXPORT_MODULE()
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
 RCT_EXPORT_METHOD(groth16_prover:(NSString *)zkeyBytes1
                   witnessData:(NSString *)wtnsBytes1
                   resolve:(RCTPromiseResolveBlock)resolve
@@ -23,39 +14,30 @@ RCT_EXPORT_METHOD(groth16_prover:(NSString *)zkeyBytes1
   CFTimeInterval startTime = CACurrentMediaTime(); // Capture start time
 
     RCTLogInfo(@"groth16_prover called");
-    // 22,960,382
-    // 28795369
-    // 22960382
-    // Assign the NSData objects to the appropriate C++ function parameters:
+
+    // TODO: we should accept just the bytes as input, not base64 encoded strings
     // NSData decode base64
     NSData* zkeyBytes = [[NSData alloc]initWithBase64EncodedString:zkeyBytes1 options:0];
     NSData* wtnsBytes = [[NSData alloc]initWithBase64EncodedString:wtnsBytes1 options:0];
 
-    // NSData *zkeyBytes = [[NSData alloc] initWithBase64EncodedString:zkeyBase64 options:0];
-//    NSData *wtnsBytes = [[NSData alloc] initWithBase64EncodedString:witnessBase64 options:0];
-
     const void *zkey_buffer = [zkeyBytes bytes];
     unsigned long zkey_size = [zkeyBytes length];
-    RCTLogInfo(@"groth16_prover zkey_size: %lu", zkey_size);
 
     const void *wtns_buffer = [wtnsBytes bytes];
     unsigned long wtns_size = [wtnsBytes length];
-    RCTLogInfo(@"groth16_prover wtns_size: %lu", wtns_size);
 
     char proof_buffer[16384];
     unsigned long proof_size = 16384;
-    RCTLogInfo(@"groth16_prover 1");
 
     char public_buffer[16384];
     unsigned long public_size = 16384;
-    RCTLogInfo(@"groth16_prover 2");
 
     char error_msg[256];
     unsigned long error_msg_maxsize = 256;
-    RCTLogInfo(@"groth16_prover 3");
+
     groth16_prover(zkey_buffer, zkey_size, wtns_buffer, wtns_size, proof_buffer, &proof_size, public_buffer, &public_size, error_msg, error_msg_maxsize);
 
-     CFTimeInterval endTime = CACurrentMediaTime(); // Capture end time
+    CFTimeInterval endTime = CACurrentMediaTime(); // Capture end time
     RCTLogInfo(@"Execution time: %f seconds", endTime - startTime); // Log the execution time
 
     RCTLogInfo(@"groth16_prover 4");
