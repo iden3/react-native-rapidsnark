a9393c7fa32c90456af42c1ec1a74e0a06d3a4d9
Yaroslav <morya.yaroslav@gmail.com>
2023-11-24 19:33:07 +0100

    Added bit of docs and polished.

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # react-native-rapidsnark
 
-React-native wrapper for rapidsnark prover library.
+---
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![npm version](https://badge.fury.io/js/react-native-rapidsnark.svg)](https://badge.fury.io/js/react-native-rapidsnark)
+
+React Native wrapper for rapidsnark prover library.
+Currently only supports groth16 prover.
 
 ## Installation
 
 ```sh
 npm install react-native-rapidsnark
 ```
+
+#### iOS linking troubleshooting
+
+If you get an error like this:
+
+```
+building for 'iOS-simulator', but linking in object file (${SRC_ROOT}/ios/Frameworks/Rapidsnark.xcframework/ios-arm64/librapidsnarkmerged.a[2](fq.o)) built for 'iOS'
+```
+
+1. Open XCode
+2. Select your project in the left sidebar
+3. Select `Build Phases` tab
+4. Expand `Link Binary With Libraries`
+5. Add `RapidSnark.xcframework` from `Workspace/Pods` folder.
 
 ## Usage
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import { NativeModules, Platform } from 'react-native';
+import {NativeModules, Platform} from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-rapidsnark' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
 const Rapidsnark = NativeModules.Rapidsnark
   ? NativeModules.Rapidsnark
   : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+    {},
+    {
+      get() {
+        throw new Error(LINKING_ERROR);
+      },
+    }
+  );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Rapidsnark.multiply(a, b);
+export function groth16_prover(zkey: string, witness: string): Promise<number> {
+  return Rapidsnark.groth16_prover(zkey, witness);
 }
