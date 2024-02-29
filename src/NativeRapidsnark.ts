7902d5db8f48fcc848ed22a264d42b050c5c6c26
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-02-29 18:15:57 +0100

    Fixed example. Added new arch support.

---
+++ src/NativeRapidsnark.ts
@@ -0,0 +1,35 @@
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+
+export interface Spec extends TurboModule {
+  groth16Prove: (
+    zkey: string,
+    witness: string,
+    proofBufferSize: number,
+    public_buffer_size: number,
+    errorBufferSize: number
+  ) => Promise<{ proof: string; pub_signals: string }>;
+  groth16ProveWithZKeyFilePath: (
+    zkey_path: string,
+    witness: string,
+    proofBufferSize: number,
+    public_buffer_size: number,
+    errorBufferSize: number
+  ) => Promise<{ proof: string; pub_signals: string }>;
+  groth16Verify: (
+    proof: string,
+    publicSignals: string,
+    verificationKey: string,
+    errorBufferSize: number
+  ) => Promise<boolean>;
+  groth16PublicSizeForZkeyBuf: (
+    zkey: string,
+    errorBufferSize: number
+  ) => Promise<number>;
+  groth16PublicSizeForZkeyFile: (
+    zkeyPath: string,
+    errorBufferSize: number
+  ) => Promise<number>;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('RNRapidsnark');
